0c4ee8bd9128085638d01b8585548ed2d9d32e84
Dylan ranger <dranger@c3r2s3.wethinkcode.co.za>
2020-07-08 15:22:55 +0200

    Updated comments to show understanding

--- 001-exercises/ex02/who_am_i.sh
+++ 001-exercises/ex02/who_am_i.sh
@@ -1,4 +1,4 @@
 #Save the whoami command in the search variable 
 search=`whoami`
-#perform an ldap search (use -x to use simple auth instead of SASL), use grep to narrow down the return results to dn, then cut the dn: name key out of the results. This should return only the value of the designated name. 
-ldapsearch -x "(uid=$search)" dn | grep '^dn:' | cut -c5-999
+#perform an ldap search inserting the whoami command into the search filter (use -x to use simple auth instead of SASL). Use grep to narrow down the return results to only show the designated name. Fincally, cut the dn: name key out of the results, starting from column 5 and ending in 999(a number that will safely cut the entire line). This should return only the value of the designated name. 
+ldapsearch -x "(uid=$search)" | grep '^dn:' | cut -c5-999
