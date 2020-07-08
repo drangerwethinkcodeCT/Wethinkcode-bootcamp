3f97f04669c8fcee95aa1d51b6f4293c079f4abd
Dylan ranger <dranger@c3r2s3.wethinkcode.co.za>
2020-07-08 16:21:42 +0200

    Added comments to show understanding

--- 001-exercises/ex10/MAC.sh
+++ 001-exercises/ex10/MAC.sh
@@ -6,8 +6,8 @@
 #    By: dranger <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/07/08 12:06:47 by dranger           #+#    #+#              #
-#    Updated: 2020/07/08 12:08:30 by dranger          ###   ########.fr        #
+#    Updated: 2020/07/08 16:20:51 by dranger          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
-
-ifconfig | grep ether
+#ifconfig will display the mac addresses. Filter for ether and then cut ether from results. 
+ifconfig | grep ether | cut -c 8-
