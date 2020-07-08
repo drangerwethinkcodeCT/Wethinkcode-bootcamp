c57bece9337c14b8ee4b237f226eb5db0255e839
Dylan ranger <dranger@c3r2s3.wethinkcode.co.za>
2020-07-08 08:45:30 +0200

    Fixed exercise two to actually be a shell script :'D

--- 001-exercises/ex02/who_am_i.sh
+++ 001-exercises/ex02/who_am_i.sh
@@ -1,3 +1,4 @@
-%>sh who_am_i.sh
-uid=dranger,ou=2020,ou=studene,dc=-42,dc=ct
-%>
+#Save the whoami command in the search variable 
+search=`whoami`
+#perform an ldap search (use -x to use simple auth instead of SASL), use grep to narrow down the return results to dn, then cut the dn: name key out of the results. This should return only the value of the designated name. 
+ldapsearch -x "(uid=$search)" dn | grep '^dn:' | cut -c5-999
