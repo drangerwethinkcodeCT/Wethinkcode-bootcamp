2d25e32a187de9c6b38a2f8d1b380504a9d44a9d
Dylan ranger <dranger@c3r2s3.wethinkcode.co.za>
2020-07-08 16:39:53 +0200

    Added comments for skip.sh

--- 001-exercises/ex14/skip.sh
+++ 001-exercises/ex14/skip.sh
@@ -6,7 +6,7 @@
 #    By: dranger <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/07/08 13:31:05 by dranger           #+#    #+#              #
-#    Updated: 2020/07/08 14:03:45 by dranger          ###   ########.fr        #
+#    Updated: 2020/07/08 16:38:37 by dranger          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #awk will make sure only odd lines is printed. The number to the left of the == operator is for the line count while the 1 is the line where the list starts printing from.
