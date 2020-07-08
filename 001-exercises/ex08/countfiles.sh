4c27638a9e09a67a9c79914985ebe26c185434bc
Dylan ranger <dranger@c3r2s3.wethinkcode.co.za>
2020-07-08 16:09:18 +0200

    Updated comments to show understanding

--- 001-exercises/ex08/countfiles.sh
+++ 001-exercises/ex08/countfiles.sh
@@ -6,7 +6,7 @@
 #    By: dranger <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/07/08 13:22:34 by dranger           #+#    #+#              #
-#    Updated: 2020/07/08 15:56:20 by dranger          ###   ########.fr        #
+#    Updated: 2020/07/08 16:08:51 by dranger          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #Find all files from the current directory that are either files or directories. If the last component of the path name contains a file extension with characters following the full stop, include that file. Prune will ensure that hidden files and folders are not included in the results. wc -l ensures that the number of files and not the path is returned in the result.
