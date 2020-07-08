7f37f3d5a6c46cc48dcce9e5b132641e61d3559f
Dylan ranger <dranger@c3r2s3.wethinkcode.co.za>
2020-07-08 15:27:30 +0200

    Added comments to show understanding

--- 001-exercises/ex06/find_sh.sh
+++ 001-exercises/ex06/find_sh.sh
@@ -6,8 +6,8 @@
 #    By: dranger <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/07/08 10:59:58 by dranger           #+#    #+#              #
-#    Updated: 2020/07/08 11:09:56 by dranger          ###   ########.fr        #
+#    Updated: 2020/07/08 15:26:42 by dranger          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
-#List all items in this directory tree, search for only .sh files and then cut the .sh from the results.
+#List all items in this directory tree, searching for only .sh files and then cut the .sh file extension from the results.
 ls -R |grep "\.sh$" | cut -f 1 -d '.'
