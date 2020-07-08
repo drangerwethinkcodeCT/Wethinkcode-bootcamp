2aaf6733dbcc8daed92c331cd0b501ab4f440af5
Dylan ranger <dranger@c3r2s3.wethinkcode.co.za>
2020-07-08 15:21:02 +0200

    Added comments to show understanding.

--- 001-exercises/ex04/print_groups.sh
+++ 001-exercises/ex04/print_groups.sh
@@ -1,2 +1,2 @@
-#Display the groups for the FT_USER env, then use the tr or transliterate command to replace any spaces with commas
+#Display the group memberships for the FT_USER env, then use the tr or transliterate command to replace any spaces with commas
 groups $FT_USER | tr ' ' ','
