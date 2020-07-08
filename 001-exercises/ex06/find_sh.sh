5c3476629fd0f862e666e24bf038d385a1e7c71b
Dylan ranger <dranger@c3r2s3.wethinkcode.co.za>
2020-07-08 11:10:39 +0200

    updated ex06 to remove file extensions from results

--- 001-exercises/ex06/find_sh.sh
+++ 001-exercises/ex06/find_sh.sh
@@ -1,2 +1,13 @@
-#Recursively list all files in the directory tree and return only those that have a .sh extension.
-ls -R |grep "\.sh$"
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    find_sh.sh                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: dranger <marvin@42.fr>                     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/07/08 10:59:58 by dranger           #+#    #+#              #
+#    Updated: 2020/07/08 11:09:56 by dranger          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+#List all items in this directory tree, search for only .sh files and then cut the .sh from the results.
+ls -R |grep "\.sh$" | cut -f 1 -d '.'
