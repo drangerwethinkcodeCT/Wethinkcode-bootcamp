28efadc58a6453dfbf50bec6da6e3715ee9ea756
drangerwethinkcodeCT <67954211+drangerwethinkcodeCT@users.noreply.github.com>
2020-07-09 17:18:56 +0200

    Update r_dwssap.sh

--- 001-exercises/ex16/r_dwssap.sh
+++ 001-exercises/ex16/r_dwssap.sh
@@ -9,18 +9,4 @@
 #    Updated: 2020/07/08 13:19:19 by dranger          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
-
-
-export FT_LINE1=12
-export FT_LINE2=20   
-cat /etc/passwd | \
-    grep -v '\#' | \
-    sed '1!n;d' | \
-    cut -d':' -f1 | \
-    rev | \
-    sort -r | \
-    awk 'NR>= ENVIRON["FT_LINE1"] && NR<= ENVIRON["FT_LINE2"]' | \
-    paste -s -d"," - | \
-    sed 's/,/, /g' | \
-    sed 's/$/./' | \
-    tr -d '\n'
+*incomplete
