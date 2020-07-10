a4851231089c798670df9940dc7facb4f2c857a3
drangerwethinkcodeCT <67954211+drangerwethinkcodeCT@users.noreply.github.com>
2020-07-10 09:14:26 +0200

    Update ft_print_alphabet.c

--- 001-exercises/day2/ex00/ft_print_alphabet.c
+++ 001-exercises/day2/ex00/ft_print_alphabet.c
@@ -1,10 +1,21 @@
+
+#include <unistd.h>
+
+// Prototype the function:
 void ft_print_alphabet(void);
 void ft_putchar(char c);
 
+void ft_putchar(char c){
+    write(1, &c, 1);
+}
+
 void    ft_print_alphabet(void)
 {
+    // Create a variable that will act as a starting point and a counter.
     char    alphabetCount;
+    // Initialize variable to start with the letter a:
     alphabetCount = 'a';
+    // Loop through the alphabet, printing each character from a - z
     while (alphabetCount <= 'z')
     {
        ft_putchar(alphabetCount);
