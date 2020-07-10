c9b9fa9f9c8aaacb0eadadcfb8520caf95e63536
drangerwethinkcodeCT <67954211+drangerwethinkcodeCT@users.noreply.github.com>
2020-07-10 09:23:25 +0200

    Update ft_print_numbers.c

--- 001-exercises/day2/ex02/ft_print_numbers.c
+++ 001-exercises/day2/ex02/ft_print_numbers.c
@@ -1,10 +1,29 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_print_numbers.c                                 :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: dranger <marvin@42.fr>                     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/07/08 13:31:05 by dranger           #+#    #+#              #
+#    Updated: 2020/07/08 16:38:37 by dranger          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+#include <unistd.h>
+// initialize the functions:
 void ft_print_numbers(void);
 void ft_putchar(char c);
 
+void ft_putchar(char c){
+    write(1, &c, 1);
+}
+
 void ft_print_numbers()
 {
+    // Create a variable to start counting from 0.
     char c;
     c = '0';
+    // Loop through the numbers starting from 0 through to nine.
     while (c <= '9')
     {
         ft_putchar(c);
