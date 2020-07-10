980a0e00de1211962ce713698d7eb2d47384b690
drangerwethinkcodeCT <67954211+drangerwethinkcodeCT@users.noreply.github.com>
2020-07-10 09:19:06 +0200

    Update who_am_i.sh

--- 001-exercises/ex02/who_am_i.sh
+++ 001-exercises/ex02/who_am_i.sh
@@ -1,3 +1,15 @@
+ 
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    whoami.sh                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: dranger <marvin@42.fr>                     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/07/08 13:31:05 by dranger           #+#    #+#              #
+#    Updated: 2020/07/08 16:38:37 by dranger          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
 #Save the whoami command in the search variable 
 search=`whoami`
 #perform an ldap search inserting the whoami command into the search filter (use -x to use simple auth instead of SASL). Use grep to narrow down the return results to only show the designated name. Fincally, cut the dn: name key out of the results, starting from column 5 and ending in 999(a number that will safely cut the entire line). This should return only the value of the designated name. 
