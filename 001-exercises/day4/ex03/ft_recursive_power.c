1ffc24ec367d7a601896c2755038be681506fa54
Dylan ranger <dranger@c3r2s3.wethinkcode.co.za>
2020-07-13 12:13:45 +0200

    day4ex03, recursive exponent

--- 001-exercises/day4/ex03/ft_recursive_power.c
+++ 001-exercises/day4/ex03/ft_recursive_power.c
@@ -6,25 +6,25 @@
 /*   By: dranger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/12 15:00:11 by dranger           #+#    #+#             */
-/*   Updated: 2020/07/13 12:10:18 by dranger          ###   ########.fr       */
+/*   Updated: 2020/07/13 12:12:55 by dranger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 int ft_recursive_power(int nb, int power)
 {
-if (power < 0 || (nb == 0 && power != 0))
+	if (power < 0 || (nb == 0 && power != 0))
     {
         return (0);
     }
 
-if (power == 0)
+	if (power == 0)
     {
         return (1);
     }
 
-if (power == 1)
+	if (power == 1)
     {
         return (nb);
     } else {
-return (nb * ft_recursive_power(nb, power - 1));
+		return (nb * ft_recursive_power(nb, power - 1));
     }
 }
